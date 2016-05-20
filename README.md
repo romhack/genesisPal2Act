d7268ae460e8e4647049e8024a1f6595d0cc481a
Griever <romhack@gmail.com>
2016-05-20 20:56:25 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+genesisPal2Act - a tool for conversion from generic 9bpp Sega Genesis palette to 24 bpp ACT format
+=========
+ 
+Simple tool to convert ripped raw palette from Sega Genesis format to 24 bpp ACT palette. Generic palette is usually ripped from emulator savestate or debug tool. ACT is usually recognized by photoshop, graphics gale or specific romhacking tool, like Djinn's [Map image](http://chief-net.ru/index.php?option=com_content&task=view&id=471&Itemid=55).
+
+```
+Usage: genesisPal2Act [-v] [FILE]
+  FILE    input file of raw palette
+  -v      show version number
+```
+
+Use [Haskell stack](http://docs.haskellstack.org/en/stable/install_and_upgrade/) to compile the tool.
---
+++ src/Main.hs
@@ -0,0 +1,25 @@
+module Main where
+
+
+import qualified Data.Bitstream.Lazy as Bi
+import           Data.List.Split
+import           System.Environment
+
+main :: IO()
+main = getArgs >>= parse
+parse ["-v"] = putStrLn "genesisPal2Act tool 0.1"
+parse [name] = convert name
+parse _ = putStrLn "Usage: genesisPal2Act [-v] [FILE]"
+
+convert :: String -> IO()
+convert name = do
+  i <-   Bi.readFile name--load and divide by nybbles
+  let
+    input = chunksOf 4 $ Bi.unpack (i :: Bi.Bitstream Bi.Right)
+    out = conv input
+  Bi.writeFile "palAct.bin" (Bi.pack out :: Bi.Bitstream Bi.Right)
+
+conv :: [[Bool]] -> [Bool]
+conv [] = []
+conv (nul: b: g: r: rest) = r ++ tailAlign ++ g ++ tailAlign ++ b ++ tailAlign ++ conv rest
+  where tailAlign = [False, False, False, False] --align value with zeros
